3b132893b17a411119bd94e20238e3b4b2bf140a
Yudai Udagawa <nimius.rd@gmail.com>
2021-07-26 00:07:10 +0900

    Fix test

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,24 +7,22 @@ from fastapi.testclient import TestClient
 
 from src.main import app
 
+client = TestClient(app)
+path_to_dir = Path(__file__).parent
+path_to_data = path_to_dir / "api" / "data"
+
 
 class MainTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        path_to_dir = Path(__file__).parent
-        cls.path_to_data = path_to_dir / "api" / "data"
-        cls.client = TestClient(app)
-
     def test_input1(self):
-        with open(self.path_to_data / "input1.json") as fp:
+        with open(path_to_data / "input1.json") as fp:
             json_input1 = json.load(fp)
 
-        correct_response = self.client.post("/", json=json_input1)
-        correct_status_code = correct_response.status_code
+        response = client.post("/", json=json_input1)
+        status_code = response.status_code
 
-        with open(self.path_to_data / "output1.json") as fp:
+        with open(path_to_data / "output1.json") as fp:
             json_output1 = json.load(fp)
 
         self.maxDiff = None
-        self.assertEqual(correct_status_code, status.HTTP_200_OK)
-        self.assertEqual(correct_response.json(), json_output1)
+        self.assertEqual(status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), json_output1)
