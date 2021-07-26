737b01d1c426c882ee68efd77ab2b15722e13d14
Yudai Udagawa <nimius.rd@gmail.com>
2021-07-26 18:21:23 +0900

    Fix typo

--- src/api/some_solver.py
+++ src/api/some_solver.py
@@ -1,12 +1,12 @@
 from src.api.model import input, output
-from src.solver import some_sovler
+from src.solver import some_solver
 
 apiName = "Solver"
 apiVersion = "1.0.0"
 
 
 def process(input: input.SchemaOfSolverInput) -> output.SchemaOfSolverOutput:
-    result = some_sovler.analyze(input.name)
+    result = some_solver.analyze(input.name)
     response = output.SchemaOfSolverOutput(
         apiName=apiName,
         apiVersion=apiVersion,
