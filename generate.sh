12ca2afd13071dc4a7e7afc6f1b7f39d658ab3c9
Yudai Udagawa <nimius.rd@gmail.com>
2021-07-26 17:40:41 +0900

    Fix script

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
-/usr/src/app/.local/bin/datamodel-codegen  --input src/api/model/schema/metadata.json --input-file-type jsonschema --output src/api/model/metadata.py
-/usr/src/app/.local/bin/datamodel-codegen  --input src/api/model/schema/error.json --input-file-type jsonschema --output src/api/model/error.py
-/usr/src/app/.local/bin/datamodel-codegen  --input src/api/model/schema/input.json --input-file-type jsonschema --output src/api/model/input.py
-/usr/src/app/.local/bin/datamodel-codegen  --input src/api/model/schema/output.json --input-file-type jsonschema --output src/api/model/output.py
+/usr/src/app/.local/bin/datamodel-codegen  --input src/api/model/schema/metadata.json --input-file-type jsonschema --output src/api/model/metadata.py --field-constraints
+/usr/src/app/.local/bin/datamodel-codegen  --input src/api/model/schema/error.json --input-file-type jsonschema --output src/api/model/error.py --field-constraints
+/usr/src/app/.local/bin/datamodel-codegen  --input src/api/model/schema/input.json --input-file-type jsonschema --output src/api/model/input.py --field-constraints
+/usr/src/app/.local/bin/datamodel-codegen  --input src/api/model/schema/output.json --input-file-type jsonschema --output src/api/model/output.py --field-constraints
