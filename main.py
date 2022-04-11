82a2afde1504e528a649b27cfc69195472cec269
Yudai Udagawa <nimius.rd@gmail.com>
2022-04-12 01:20:04 +0900

    Update

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import functions_framework
 
 from src.example.solvers import SomeSolver
 
+
 @functions_framework.http
 def apply_some_solver(request):
-    # content_type = request.headers["content-type"]
-    # if "application/json" in content_type:
-    #     request_json = request.get_json(silent=True)
-    # else:
-    #     raise ValueError("Unknown content type: {}".format(content_type))
-    # print(request_json)
-    # response_data = jsonify(SomeSolver().process(request_json))
+    content_type = request.headers["content-type"]
+    if "application/json" in content_type:
+        request_json = request.get_json(silent=True)
+    else:
+        raise ValueError("Unknown content type: {}".format(content_type))
+    print(request_json)
+    response_data = jsonify(SomeSolver().process(request_json))
 
-    # return response_data
-    return "hello"
+    return response_data
