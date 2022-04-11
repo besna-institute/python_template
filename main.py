6d92160dd3e5c76e55dd3a7c1ab3639eb332ac0c
Yudai Udagawa <nimius.rd@gmail.com>
2022-04-11 21:18:58 +0900

    Update

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ def apply_some_solver(request: Request):
 
     # return response_data
     return "hello"
+
+
+def hello_world(request):
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return f'Hello World!'
