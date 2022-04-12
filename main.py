0786d9abfca8346297a855721d4d5f6e3d52182d
Yudai Udagawa <nimius.rd@gmail.com>
2022-04-12 12:23:01 +0900

    Update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import functions_framework
 from flask import Request, jsonify
 
 from src.example.solvers import SomeSolver
+from src.models import Input
 
 
 @functions_framework.http
@@ -12,6 +13,6 @@ def apply_some_solver(request: Request):
     else:
         raise ValueError("Unknown content type: {}".format(content_type))
     print(request_json)
-    response_data = jsonify(SomeSolver().process(request_json))
+    response_data = jsonify(SomeSolver().process(Input.from_dict(request_json)))
 
     return response_data
