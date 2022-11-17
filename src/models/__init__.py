3e97f1846940a7842470ccaa8f725914f989d3e3
Yudai Udagawa <nimius.rd@gmail.com>
2022-11-17 18:55:10 +0900

    Fix example code

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,3 +3,8 @@ from src.models.input import Input  # noqa: F401
 from src.models.output import Output  # noqa: F401
 
 api_version = "1.0.0"
+__all__ = [
+    "Error",
+    "Input",
+    "Output",
+]
