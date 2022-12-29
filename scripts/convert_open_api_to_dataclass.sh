f489cd136cc0cd7c29baf3efb5b217ddb64f6683
Yudai Udagawa <nimius.rd@gmail.com>
2022-12-30 01:53:55 +0900

    Update openapi-generator

--- scripts/convert_open_api_to_dataclass.sh
+++ scripts/convert_open_api_to_dataclass.sh
@@ -7,5 +7,6 @@ rm -fr "$PROJECT_ROOT/src/models/"*
 cp -r generated/src/models/* "$PROJECT_ROOT/src/models/"
 rm -fr generated
 cd "$PROJECT_ROOT" || exit
+python -m pyupgrade --py310-plus src/models/*.py
 python -m autoflake -i -r --remove-all-unused-imports src/models
 python -m black src/models
