164d564c6d0ca9b3c1cd02910c61f56dad693ce7
Yudai Udagawa <nimius.rd@gmail.com>
2022-12-29 20:57:07 +0900

    Fix apply_template_updates.sh

--- scripts/apply_template_updates.sh
+++ scripts/apply_template_updates.sh
@@ -12,25 +12,7 @@ cd "$(git rev-parse --show-toplevel)"
 git remote add template https://github.com/besna-institute/python_template.git
 git fetch --no-tags template refs/tags/v*:refs/tags/template-v*
 TEMPALTE_VERSION=$(git tag --sort=-v:refname -l 'template-v*' | head -1)
-git diff --output=update.patch main "$TEMPALTE_VERSION" -- \
-  .devcontainer \
-  .editorconfig \
-  .flake8 \
-  .gcloudignore \
-  .gitignore \
-  .github/workflows \
-  .pylintrc \
-  .vscode \
-  Dockerfile \
-  cloudbuild.yaml \
-  openapi_generator.yaml \
-  openapitools.json \
-  pyproject.toml \
-  requirements.in \
-  scripts \
-  src/__init.py \
-  template
-
+git diff --output=update.patch main "$TEMPALTE_VERSION"
 echo "$(tput setaf 2)✓ Create patch from $TEMPALTE_VERSION"
 git remote rm template
 git tag | grep template-v | xargs git tag -d
