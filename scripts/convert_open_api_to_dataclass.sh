37dfd808d8b3a9d07dc5d80c39f542867033f447
Yudai Udagawa <nimius.rd@gmail.com>
2023-06-19 17:07:11 +0900

    Move install script for openjdk-17-jre

--- scripts/convert_open_api_to_dataclass.sh
+++ scripts/convert_open_api_to_dataclass.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "Installing openjdk-17-jre..."
+sudo apt-get update
+sudo apt-get install -y openjdk-17-jre
+
+echo
+echo "Generating code from OpenAPI spec..."
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 cd "$PROJECT_ROOT/openapi-generator" || exit
 npm exec -y -- @openapitools/openapi-generator-cli generate -c openapi_generator.yaml
@@ -7,6 +13,9 @@ rm -fr "$PROJECT_ROOT/src/models/"*
 cp -r generated/src/models/* "$PROJECT_ROOT/src/models/"
 cp generated/src/util.py "$PROJECT_ROOT/src/models/_util.py"
 rm -fr generated
+
+echo
+echo "Formatting code..."
 cd "$PROJECT_ROOT" || exit
 python -m pyupgrade --py310-plus src/models/*.py
 python -m autoflake -i -r --remove-all-unused-imports src/models
