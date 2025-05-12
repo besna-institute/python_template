6122134bcf03b09cd94ae417c90769d67f85570f
Yudai Udagawa <nimius.rd@gmail.com>
2025-05-12 14:09:58 +0000

    アプリケーションの実行ホストを「0.0.0.0」から「127.0.0.1」に変更

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def example_endpoint() -> Response:
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(host="127.0.0.1", port=int(os.environ.get("PORT", 8080)))
